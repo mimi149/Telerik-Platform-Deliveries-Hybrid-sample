4223f63bf412c2836799343ff8885c1df3746e28
Tran Nguyen <baotranmimi@gmail.com>
2016-08-10 22:14:15 +0000

    Changes made by Tran Nguyen (baotranmimi@gmail.com).

--- backend-services-offline-hybrid-sample/config.js
+++ backend-services-offline-hybrid-sample/config.js
@@ -2,6 +2,6 @@
 //WARNING: You must never include your master key in your app. We only use it
 //in order to initialize the required server-side items for the demo to function.
 Config = {
-    AppId: 'your-app-id-here',
-    MasterKey: 'your-master-key-here' 
+    AppId: 'jotbyso2n6ooqjg1',
+    MasterKey: 'wX4QFytwojeGEDrTkBVZrVvR6PiI3pxM' 
 };
